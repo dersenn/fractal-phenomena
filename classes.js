96239ef66aac2b4e2b3ffba03b0ad56ef9634f3f
Boris Périsset <sehroiber@gmail.com>
2021-11-12 23:17:51 +0100

    Baumsammlung ist fast fertig.

--- classes.js
+++ classes.js
@@ -19,24 +19,29 @@ class Cell {
 
 class Tree {
     constructor(treeSize) {
+        //Eröffnet den Baum als Liste
         this.tree = [];
+
         this.midPoint = treeSize / 2
         // Begin Vector Zeichnung (Vector (x, y, z) allesamt Optional)
         this.begin = createVector(this.midPoint, this.midPoint*1.5); //der Beginn ist pro Zelle x mittig unten
         this.end = createVector(this.midPoint, this.midPoint); //das Ende ist pro Zelle x mittig bis ins Zentrum
         // Anzahl Äste am Baum
         this.twig = random(1,50)
-
+        // Der Baum wird initialisiert
         this.root = new Branch(this.begin, this.end);
     } 
     grow() {
+        // Der Stamm des Baums
         this.tree[0] = this.root;
-      
+        // Die Äste des Baums
         for (var i =  0 ; i < this.twig; i++) {
             this.tree.push(this.tree[i].branchA());
             this.tree.push(this.tree[i].branchB());
             this.tree[i].show();
-            this.tree[i].angleRotation();
+
+            // Warum funktioniert dies nicht??? Es sollte den Winkel als Sinuskurve schwingen lassen.
+            this.tree[i].winkelRotation();
         }
     }
 }
@@ -46,26 +51,27 @@ class Branch {
         this.begin = begin;
         this.end = end;
         //Winkelspiel
-        this.a = random(0.5/0.02);
-        this.increment = TAU / 360;
-        this.angle = sin(this.a);
+        this.a = random(0.5/0.02) // eigentlich sollte es bei 0.0 anfangen und dasn animieren...
+        this.increment = TAU / random(25/360);
+        this.winkel = sin(this.a);
         //Farbspiel
         this.green = random(150,250);
         this.blue = random(150,250);
         //Ast A
         this.branchA = function() {
             let dir = p5.Vector.sub(this.end, this.begin);
-            dir.rotate(this.angle)
+            dir.rotate(this.winkel)
             dir.mult(0.45)
             let newEnd = p5.Vector.add(this.end, dir)  
             let b = new Branch (this.end, newEnd)
             
             return b;
         }
-        //Ast A
+        //Ast B
         this.branchB = function() {
+            
             let dir = p5.Vector.sub(this.end, this.begin);
-            dir.rotate(-this.angle)
+            dir.rotate(-this.winkel)
             dir.mult(0.45)
             let newEnd = p5.Vector.add(this.end, dir)  
             let b = new Branch (this.end, newEnd)
@@ -80,7 +86,9 @@ class Branch {
         circle(this.begin.x, this.begin.y-25, random(2,5)) // proof obs bis hier hin funktioniert
     }
 
-    angleRotation() {
+    // wie kann ich diese Funktion zum leben erwecken?
+
+    winkelRotation() {
         this.a = this.a + this.increment;
       }
 
